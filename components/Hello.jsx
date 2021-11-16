928691d24c8e63766d73601a6504715836fd1da0
Adiazx14 <adrianvaldes1439@gmail.com>
2021-11-15 19:53:58 -0500

    first media queries

--- components/Hello.jsx
+++ components/Hello.jsx
@@ -8,17 +8,20 @@ const Hello = ()=> {
                 </h1>
                 </header>
                 <main>
-                <h2 className="red">
+                    <div className="flex-div">
+                    <h2 className="red">
                 
-                    &#60;Full-Stack Developer
+                &#60;Full-Stack Developer
+                <br />
                 
-                </h2>
-
-                <p> <span className="red">
-                    location=</span>&quot;Miami, Florida&quot;</p>
-
-                <p><span className="red">description=</span>&quot;I am a self-taught full-stack web developer who works as a freelancer, mainly with Django in the back-end and Nextjs in the front-end. I love  building everything custom: having fun implementing every feature and creating the most reliable, scalable, and personalized solutions out of my customers’ needs.&quot; <span className="red">/&#62;</span></p>
-           <Image src="/ShortArrow.svg" height="54" width="54"/>
+            </h2>
+            <p><span className="red">
+            location= </span>"Miami, Florida"</p>
+            
+            <p><span className="red">description=</span> "I am a self-taught full-stack web developer who works as a freelancer, mainly with Django in the back-end and Nextjs in the front-end. I love  building everything custom: having fun implementing every feature and creating the most reliable, scalable, and personalized solutions out of my customers’ needs." <span className="red">/&#62;</span></p>
+       <Image src="/ShortArrow.svg" height="54" width="54"/>
+                    </div>
+                
            </main>
 
         </section>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-
+import '../styles/media queries.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
