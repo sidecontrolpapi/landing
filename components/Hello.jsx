5b1389837019875ac9c6bcb29ca78b4ff995a2f2
Adiazx14 <adrianvaldes1439@gmail.com>
2021-11-15 20:00:50 -0500

    quotes again

--- components/Hello.jsx
+++ components/Hello.jsx
@@ -16,9 +16,9 @@ const Hello = ()=> {
                 
             </h2>
             <p><span className="red">
-            location= </span>"Miami, Florida"</p>
+            location= </span>&quot;Miami, Florida&quot;</p>
             
-            <p><span className="red">description=</span> "I am a self-taught full-stack web developer who works as a freelancer, mainly with Django in the back-end and Nextjs in the front-end. I love  building everything custom: having fun implementing every feature and creating the most reliable, scalable, and personalized solutions out of my customers’ needs." <span className="red">/&#62;</span></p>
+            <p><span className="red">description=</span> &quot;I am a self-taught full-stack web developer who works as a freelancer, mainly with Django in the back-end and Nextjs in the front-end. I love  building everything custom: having fun implementing every feature and creating the most reliable, scalable, and personalized solutions out of my customers’ needs.&quot; <span className="red">/&#62;</span></p>
        <Image src="/ShortArrow.svg" height="54" width="54"/>
                     </div>
                 
