1fbffe7f642e322db0921ff22b084775ae9eb075
Adiazx14 <adrianvaldes1439@gmail.com>
2022-02-03 20:48:34 -0500

    update About Me summary

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -7,7 +7,7 @@ const Links = ({mobile}) => {
 
     return (
     <>
-     <li  className={`Navlink ${selected>=0.7&&selected<=1.8?"selected":""}`}> <span onClick={()=>handleSelect(1, "Me", mobile)}>About Me</span>  </li>
+     <li  className={`Navlink ${selected>=0.7&&selected<=1.8?"selected":""}`}> <span onClick={()=>handleSelect(1, "AboutMeh1", mobile)}>About Me</span>  </li>
            <li  className={`Navlink ${selected<4.8&&selected>1.8?"selected":""}`}> <span onClick={()=>handleSelect(2, "MidArrow", mobile)}>Projects</span></li>
            <li className={`Navlink ${selected>=4.8?"selected":""}`}> <span onClick={()=>handleSelect(5, "Contact", mobile)}>Contact</span> </li> 
     </>
