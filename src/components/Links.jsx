17bd119c1396be5fb16c5d31fa61862d981681d2
adiazx14 <adrianvaldes1439@gmail.com>
2023-02-15 17:23:19 -0600

    update links

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { MenuContext } from "../context/MenuContext"
+import Pdf from "../Adrian Valdes Diaz's Resume.docx.pdf"
 
 const Links = ({mobile}) => {
     const { selected, handleSelect} = useContext(MenuContext)
@@ -7,7 +8,7 @@ const Links = ({mobile}) => {
 
     return (
     <>
-        <li className="Navlink"><a href="Resume Adrian Valdes Diaz.docx.pdf" download><span>Resume</span></a></li>
+        <li className="Navlink"><a href={Pdf} download><span>Resume</span></a></li>
         <li  className={`Navlink ${selected>=0.7&&selected<=1.8?"selected":""}`}> <span onClick={()=>handleSelect(1, "Me", mobile)}>About Me</span>  </li>
         <li  className={`Navlink ${selected<4&&selected>1.8?"selected":""}`}> <span onClick={()=>handleSelect(2, "MidArrow", mobile)}>Projects</span></li>
         <li className={`Navlink ${selected>=4?"selected":""}`}> <span onClick={()=>handleSelect(5, "Contact", mobile)}>Contact</span></li> 
